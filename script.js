915b7ea34b8a76fae02159c90361554e46bba2a5
riteshbisht1106-ui <riteshbisht1106@gmail.com>
2026-04-01 20:01:22 +0530

    update api url

--- script.js
+++ script.js
@@ -111,7 +111,7 @@ if (contactForm) {
         submitBtn.disabled = true;
 
         try {
-            const response = await fetch('http://127.0.0.1:5000/api/contact', {
+            const response = await fetch('https://portfolio-qxfs.onrender.com/api/contact', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
